9a70472c77d406811f5c4cac6bf94e7f0c7eb921
amy-f <frappier.amelie@gmail.com>
2016-04-15 12:44:31 -0400

    Début structure PHP
    Initialisation de la structure du site.

---
+++ index.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Amélie
+ * Date: 2016-04-15
+ * Time: 12:27
+ */
+?>
+
+<!DOCTYPE html>
+
+<!-- /**************************************************************************************************/
+/* Fichier ...................... : index.html */
+/* Type ......................... : Document HTML 5 */
+/* Titre ........................ : Exercice JavaScript HTML5 */
+/* Auteur ....................... : ©2016 Amélie Frappier, Cégep de Sherbrooke */
+/* Date de création ............. : 2016-04-08 */
+/* Date de mise en ligne ........ : Jamais */
+/* Date de mise à jour .......... : 2016-04-08 */
+/*******************************************************************************************************/
+/* Exercice sur l'utilisation des Regex en JavaScript dans le cadre d'un formulaire. */
+/*******************************************************************************************************/
+-->
+
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>ÉTICS LAN - Index</title>
+</head>
+    <body>
+
+    </body>
+</html>
