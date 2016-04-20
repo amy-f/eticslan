72e6430b22caf653ae2722027e201f12f6bb8a6b
Hailtothebreak <hailtothebreak@hotmail.com>
2016-04-20 10:24:24 -0400

    Started header

--- index.php
+++ index.php
@@ -29,7 +29,39 @@
 	<link rel="stylesheet" href="css/style.css" />
 
 	<style>
-			
+		.backgroundDiv
+		{
+			background-color:#000000;
+			background-image:url(img/controller2.bmp);
+			background-size:100%;
+			background-repeat: no-repeat;
+		}
+		
+		header
+		{
+			padding-left:10%;
+			padding-right:10%;
+			padding-top:60px;
+		}
+		
+		.eticsLogo
+		{
+			width:20%;
+		}
+		
+		.microboutiqueLogo
+		{
+			width:12%;
+			float:right;
+			padding-top:35px;
+		}
+		
+		.oeildechat
+		{
+			width:20%;
+			float:right;
+			padding-top:30px;
+		}
 	</style>
 	
 </head>
@@ -37,10 +69,19 @@
 		<?php include 'php\navbar.php' ?>
 		
 		
-		<div style="background-color:#000000;
-	background-image:url(img/controller2.bmp);
-	background-size:100%;
-	background-repeat: no-repeat;height:600px">
+		<div class="backgroundDiv">
+		
+		
+			<header>
+			
+				<img src="img/logolan.png" class="eticsLogo"/>
+				
+				<img src="img/oeildechat.png" class="microboutiqueLogo"/>
+				<img src="img/microboutique.png" class="oeildechat"/>
+
+			
+			
+			</header>
 		
 		
 		</div>
