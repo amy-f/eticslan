72469285872fa2efde3484972478df7a5f56faa3
Hailtothebreak <hailtothebreak@hotmail.com>
2016-04-19 23:09:40 -0400

    Footer CSS cleanup and PHP include
    Cleaned the CSS and made the include.
    
    to do: make the footer responsive.

--- index.php
+++ index.php
@@ -28,55 +28,14 @@
     <title>ÉTICS LAN - Index</title>
 	<link rel="stylesheet" href="css/style.css" />
 
+	<style>
+			
+	</style>
+	
 </head>
     <body style="background-color:#00887A"><!-- style="background-color:#3C4140" -->
 		<?php include 'php\navbar.php' ?>
 		
-		<footer style="background-color:#3C4140; width:100%;
-		padding-left:30px; padding-top:30px">
-
-			<div style="float:left; width:30%;">
-			
-			<img 
-			src="img/logocegep.png" 
-			style="
-			width:55%;"/>
-			
-			<p style="
-			font-size:9pt; color:#FFFFFF; font-weight:bold;
-			font-family: Georgia;
-			text-justify: inter-word;	text-align: justify;">
-			@ÉTICS 2016. Tous réservés Photos fournies par StockSnap.io</p>
-			
-			</div>
-
-			
-			
-			
-				<ul style="list-style-type: none; font-size:9pt; color:#FFFFFF; font-weight:bold;
-			font-family: Georgia; margin-left:8%;float:left;">
-					<li style="font-size:12pt;">Commanditaires</li>
-					<li>Microboutique</li>
-					<li>AÉCS</li>
-					<li>L'Oeil de Chat</li>
-					<li>Siboire</li>
-				</ul>
-				
-			<div style="margin-left:15%;float:left;">
-			
-				<p style="font-weight:bold;font-family: Georgia;color:#FFFFFF;font-size:12pt;">
-				Suivez-nous</p>
-				
-				<span>
-				
-				
-				</span>
-			
-			</div>
-			
-			
-			<div style="clear: both"></div>
-
-		</footer>
+		<?php include 'php\footer.php' ?>	
     </body>
 </html>
---
+++ php/footer.php
@@ -0,0 +1,35 @@
+		<footer>
+
+			<div class="footerLeftSection">
+			
+				<img src="img/logocegep.png" />
+			
+				<p>@ÉTICS 2016. Tous réservés Photos fournies par StockSnap.io</p>
+			
+			</div>
+
+			
+			<ul class="footerMiddleSection">
+				<li>Commanditaires</li>
+				<li>Microboutique</li>
+				<li>AÉCS</li>
+				<li>L'Oeil de Chat</li>
+				<li>Siboire</li>
+			</ul>
+				
+			<div class="footerRightSection">
+			
+				<p>Suivez-nous</p>
+				
+				<div style="">
+					<img src="img/twitchlogo.png" />
+					<img src="img/facebooklogo.png" />
+					<img src="img/twitterlogo.png" />
+				</div>
+			
+			</div>
+			
+			
+			<div style="clear: both"></div>
+
+		</footer>
