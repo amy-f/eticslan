faaefe903eae36bbd3e5cb20ea64917098985b89
amy-f <frappier.amelie@gmail.com>
2016-05-19 01:21:29 -0400

    Ajout du contenu pour apropos.php et equipe.php. Le contenu pour l'index est prêt, mais je vais attendre les changements sur l'autre branche.

--- php/content-apropos.php
+++ php/content-apropos.php
@@ -4,14 +4,27 @@
 	<div class="txt-align-center">
 		<h1 class="main-title">À propos de l'ÉTICS LAN</h1>
 		<img src="img/lan.GIF"/>
-
-
-				<div class="bob">
-					<h1>Un LAN fait pour les gamers, par des gamers!</h1>
-					<p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).</p>
-				</div>
 	</div>
-			</article>
+
+
+				<div class="bob col-11">
+					<div class="txt-align-center">
+						<h1>Un LAN fait pour les gamers, par des gamers!</h1>
+					</div>
+					<div id="columns">
+						Originalement une vieille tradition des Étudiants en Techniques de l'Informatique du Cégep de Sherbrooke (ÉTICS), l'ÉTICS LAN a repris officiellement avec sa toute première édition en 2014. Nous en sommes maintenant à la 3e édition de ce LAN Party, et entre le traditionnel tournoi de League of Legends et les autres séances de jeu libre organisées par les joueurs eux-mêmes, il y a de l'attrait pour tout les styles de gamers, du joueur occasionnel aux professionnels du e-sport.
+						<h3>C'est quoi un LAN?</h3>
+						Un LAN Party (aussi appelé LAN) est tout simplement un évènement ou des joueurs se rassemblent dans le but de jouer à des jeux vidéos en étant connectés sur le même réseau local - d'où le terme LAN, qui provient du concept de Local Area Network, ou réseau local. Certains évènements du genre, tels que l'ÉTICS LAN, sont orientés vers les tournois de jeux vidéos, où plusieurs équipes s'affrontent afin de gagner des prix.
+						<h3>Informations générales</h3>
+							Déroulement : Du 22 octobre 2016 à 10:30 au 23 octobre 2016 à 10:30 <br/>
+							Lieu : Cégep de Sherbrooke, pavillon 6 <br/>
+							Coût: 5$ le billet, peut être acheté directement des organisateurs au Cégep. <br/>
+							Amenez votre ordinateur et vos prises électriques, nous nous occupons du réseau et de la bouffe!
+					</div>
+				</div>
+			<div style="height:35px;"></div>
+
+</article>
 			
 			<!-- Used to make some paddig that will not
 			cut of the image -->
--- php/content-equipe.php
+++ php/content-equipe.php
@@ -2,11 +2,12 @@
 			
 	<div class="txt-align-center">
 		<h1>Équipe</h1>
-		<img src="img/lan_comite.jpg"/>
-	</div>
-			
-	<div class="col-11">
-		<p></p>
+		<img src="img/lan_comite.jpg"/> <br/>
+		<small>Quelques membres de l'équipe de l'ÉTICS LAN: Mylène Desrosiers, Kenny Lemay, Amélie Frappier et You Li</small>
+		<div class="col-11">
+			<p>L'équipe de l'ÉTICS LAN est composée d'étudiants - et d'anciens étudiants - en Techniques de l'informatique du Cégep de Sherbrooke. Dévoués et passionnés, ils travaillent bénévolement et ont à coeur depuis déjà trois ans la réussite de cet événement,  année après année, que ce soit en installant le réseau du LAN, en répondant à vos questions sur les réseaux sociaux, en créant des affiches publicitaires ou en contactant nos commanditaires. </p>
+			<p>Vous souhaitez faire partie de l'organisation ou apporter des suggestions? <a href="contact.php">Contactez-nous</a>!</p>
+		</div>
 	</div>
 			
 						
